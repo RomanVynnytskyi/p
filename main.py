bffb3c82d437aa5be9ec2bc53fcfdc3c5adb7d86
7ZipService <vynnytskyiroman777@gmail.com>
2022-09-10 16:11:59 +0300

    казка про кота сільського звичайного

---
+++ main.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self, the_cat_got_up, the_cat_ate, the_cat_went_for_a_walk, the_cat_crossed_the_road, the_cat_was_hit_by_the_car, the_cat_woke_up, ____________, cat):
+        self.the_cat_got_up = the_cat_got_up
+        self.the_cat_ate = the_cat_ate
+        self.the_cat_went_for_a_walk = the_cat_went_for_a_walk
+        self.the_cat_crossed_the_road = the_cat_crossed_the_road
+        self.the_cat_was_hit_by_the_car = the_cat_was_hit_by_the_car
+        self.the_cat_woke_up = the_cat_woke_up
+        self.____________ = ____________
+        self.cat = cat
+
+p1 = Person("кіт встав в нього над головою був надпис 9 житів", "кіт їсть", "кіт пішов гуляти", "кіт вийшов на дорогу", "кота збила машина", "кіт заспавнився біля лежанки в нього над головою був надпис 8 житів", "________________________________________________________", "це була казка про кота сільського звичайного")
+print(p1.the_cat_got_up)
+print(p1.the_cat_ate)
+print(p1.the_cat_went_for_a_walk)
+print(p1.the_cat_crossed_the_road)
+print(p1.the_cat_was_hit_by_the_car)
+print(p1.the_cat_woke_up)
+print(p1.____________)
+print(p1.cat)
