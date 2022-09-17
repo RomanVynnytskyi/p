c4d0d35b689313adb1e48b7c6f35fc3828c0ef15
7ZipService <vynnytskyiroman777@gmail.com>
2022-09-17 17:24:37 +0300

    THE Sims

--- main.py
+++ main.py
@@ -287,3 +287,99 @@ class Human:
         print(f"{car_indexes:^50}", "\n")
         print(f"Fuel - {self.car.fuel}")
         print(f"Strength - {self.car.strength}")
+    def is_alive(self):
+        if self.gladness < 0:
+            print("Depression...")
+            return False
+        if self.satiety < 0:
+            print("Dead...")
+            return False
+        if self.money <-500:
+            print("Bankrupt...")
+            return False
+    def live(self, day):
+        if self.is_alive() == False:
+            return False
+        if self.home is None:
+            print("Settled in the house")
+            self.get_home()
+        if self.car is None:
+            self.get_car()
+            print(f"I bought a car {self.car.brand}")
+        if self.job is None:
+            self.get_job()
+            print(f"I don't have a job, going to get a job {self.job.job} with salary {self.job.salary}")
+        self.days_indexes(day)
+        dice = random.randint(1, 4)
+        if self.satiety < 20:
+            print("I'll go eat")
+            self.eat()
+        elif self.gladness < 20:
+            if self.home.mess > 15:
+                print("I wand to chill, but there is so much mess... \n So I will clean the house")
+                self.clean_home()
+            else:
+                print("Let's chill!")
+                self.chill()
+        elif self.money < 0:
+            print("Start working")
+            self.work()
+        elif self.car.strength < 15:
+            print("I need to repair my car")
+            self.to_repair()
+        elif dice == 1:
+            print("Let's chill!")
+            self.chill()
+        elif dice == 2:
+            print("Start working")
+            self.work()
+        elif dice == 3:
+            print("Cleaning time!")
+            self.clean_home()
+        elif dice == 4:
+            print("Time for treats!")
+            self.shopping(manage="delicacies")
+    class Auto:
+        def __init__(self, brand_list):
+            self.brand = random.choice(list(brand_list))
+            self.fuel = brand_list[self.brand]["fuel"]
+            self.strength = brand_list[self.brand]["strength"]
+            self.consumption = brand_list[self.brand]["consumption"]
+        def drive(self):
+            if self.strength > 0 and self.fuel >= self.consumption:
+                self.fuel -= self.consumption
+                self.strength -= 1
+                return True
+            else:
+                print("The car cannot move")
+                return False
+class House:
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+job_list = {
+    "Java develover":
+        {"salary":50, "gladness_less": 10 },
+    "Python develover":
+        {"salary": 40, "gladness_less": 3},
+    "C++ develover":
+        {"salary": 45, "gladness_less": 25},
+    "Rust develover":
+        {"salary": 70, "gladness_less": 1},
+    }
+brands_of_car  = {
+    "BMW":{"fuel":100, "strength":100, "consumption": 6},
+    "Lada":{"fuel":50, "strength":40, "consumption": 10},
+    "Volvo":{"fuel":70, "strength":150, "consumption": 8},
+    "Ferrari":{"fuel":80, "strength":120, "consumption": 14},
+    }
+class Job:
+    def __init__(self):
+        self.job = random.choice(list(job_list))
+        self.salary = job_list[self.job]["salary"]
+        self.gladness_less = job_list[self.job]["gladness_less"]
+nick = Human(name="Nick")
+for day in range(1,8):
+    if nick.live(day) == False:
+        break
+
